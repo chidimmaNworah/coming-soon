ca627e7ab588762ba57c41590c3b8ed5b9898159
chidimmaNworah <chidimmanworah12@gmail.com>
2025-12-24 13:12:02 +0100

    first commit

---
+++ dist/js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - Abundant grace electronicals concept v6.0.7 (https://startbootstrap.com/theme/coming-soon)
+* Copyright 2013-2025 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/abundant-grace-electronicals-concept/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
